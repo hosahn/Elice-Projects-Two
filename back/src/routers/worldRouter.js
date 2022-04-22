e10ce84b46153e27648c91928b9958634d3bf706
Hosan_Lee <hosahn13@gmail.com>
2022-04-22 15:33:26 +0900

    [Create] #1 member  tierFunction

--- back/src/routers/worldRouter.js
+++ back/src/routers/worldRouter.js
@@ -5,15 +5,19 @@ const worldRouter = Router();
 
 worldRouter.get("/world_map", async (req, res) => {
   const countryName = req.body.country;
+
+  if (!countryName) {
+    res.sendStatus(201);
+  }
   const wines = await worldService.findByCountryWine({ countryName });
   const countryDescription = await worldService.findByCountryDescription({
     countryName,
   });
-  const result = { wine: wines, description: countryDescription };
 
   if (!wines || !countryDescription) {
     res.send("요청하신 정보를 찾을 수 없습니다.");
   }
+  const result = { wine: wines, description: countryDescription };
   res.send(result);
 });
 export { worldRouter };
--- back/src/services/worldService.js
+++ back/src/services/worldService.js
@@ -4,6 +4,10 @@ import { Country } from "../db/index.js";
 class worldService {
   static async findByCountryWine({ countryName }) {
     const result = await Wine.findByCountry({ countryName });
+    if (result.length == 0) {
+      //아무것도 찾지 못한 경우
+      return null; // null값 return
+    }
     return [result[0], result[1], result[2]];
   }
 
