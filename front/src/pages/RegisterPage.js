6c6e1acabe7b03e561e5c66c6dfe8512c31e8192
Seohyun Roh <radult951@gmail.com>
2022-04-22 17:03:59 +0900

    #5 Update 회원가입 기본 UI 구현

--- front/src/pages/RegisterPage.js
+++ front/src/pages/RegisterPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button, TextField, Typography } from "@mui/material";
 
 import Header from "../components/Header";
 
@@ -6,7 +8,32 @@ export default function RegisterPage() {
   return (
     <>
       <Header />
-      <div>RegisterPage</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "30px",
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="div"
+          style={{ marginBottom: "30px" }}
+        >
+          Register
+        </Typography>
+        <TextField label="email" type="email"></TextField>
+        <br />
+        <TextField label="name" type="text"></TextField>
+        <br />
+        <TextField label="pw" type="password"></TextField>
+        <br />
+        <TextField label="confirm pw" type="password"></TextField>
+        <br />
+        <Button variant="outlined">Register</Button>
+      </div>
     </>
   );
 }
