449412d2e36cf1c6c7c5e738636f166c1bdf2224
Seohyun Roh <radult951@gmail.com>
2022-04-23 11:47:38 +0900

    #7 update header

--- front/src/pages/WorldMapPage.js
+++ front/src/pages/WorldMapPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import {
   ComposableMap,
   Geographies,
@@ -7,6 +6,8 @@ import {
   ZoomableGroup,
   Marker,
 } from "react-simple-maps";
+
+import Header from "../components/Header";
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
@@ -17,26 +18,29 @@ function MapChart() {
   };
 
   return (
-    <ComposableMap style={{ maxWidth: "1000px" }}>
-      <ZoomableGroup zoom={1}>
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map(geo => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                fill="#DDD"
-                stroke="#FFF"
-                onClick={handleClick}
-              />
-            ))
-          }
-        </Geographies>
-        <Marker coordinates={[-74.006, 40.7128]}>
-          <circle r={4} fill="#F53" />
-        </Marker>
-      </ZoomableGroup>
-    </ComposableMap>
+    <>
+      <Header />
+      <ComposableMap style={{ maxWidth: "1000px" }}>
+        <ZoomableGroup zoom={1}>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map(geo => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#DDD"
+                  stroke="#FFF"
+                  onClick={handleClick}
+                />
+              ))
+            }
+          </Geographies>
+          <Marker coordinates={[-74.006, 40.7128]}>
+            <circle r={4} fill="#F53" />
+          </Marker>
+        </ZoomableGroup>
+      </ComposableMap>
+    </>
   );
 }
 
