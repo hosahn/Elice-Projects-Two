1bb6f660f101a97cbffe2829e0893527f8b9613c
Hosan_Lee <hosahn13@gmail.com>
2022-04-26 11:18:33 +0900

    [Update] detailService error fixed

--- back/src/services/detailService.js
+++ back/src/services/detailService.js
@@ -2,7 +2,6 @@ import { Wine } from "../db/index.js";
 
 class detailService {
   static async findByIndex({ index }) {
-    console.log(index);
     const result = await Wine.findByIndex({ index });
     return result;
   }
