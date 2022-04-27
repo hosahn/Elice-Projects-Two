71d5ee5bdf6cae28d5fb8f3e9bc583df37abe2f8
Hosan_Lee <hosahn13@gmail.com>
2022-04-27 09:45:54 +0900

    [Create] initial detail componenets

--- front/src/pages/DetailPage.js
+++ front/src/pages/DetailPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "../components/Header";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 
 export default function DetailPage() {
   const params = useParams();
@@ -16,28 +16,46 @@ export default function DetailPage() {
   return (
     <>
       <Header />
-      <div>DetailPage</div>
-
-      <img src={data?.result?.[0]["image"]} />
-
-      <div>
-        <p>{data?.result?.[0]["points"]}</p>
-        <p>{data?.result?.[0]["description"]}</p>
-        <p>{data?.result?.[0]["price"]}</p>
-        <p>{data?.result?.[0]["country"]}</p>
-      </div>
-      <h1>{data?.result?.[0]["title"]}</h1>
-
-      <div>
-        <img src={data?.similar?.[0]} />
-        <p>{data?.result?.[0]["similar1"]}</p>
-        <img src={data?.similar?.[1]} />
-        <p>{data?.result?.[0]["similar2"]}</p>
-        <img src={data?.similar?.[2]} />
-        <p>{data?.result?.[0]["similar3"]}</p>
+      <div style={{ display: "flex" }}>
+        <div>
+          <img src={data?.result?.[0]["image"]} />
+          <h1>{data?.result?.[0]["title"]}</h1>
+        </div>
+        <div>
+          <p>{data?.result?.[0]["points"]}</p>
+          <p>{data?.result?.[0]["description"]}</p>
+          <p>{data?.result?.[0]["price"]}</p>
+          <p>{data?.result?.[0]["country"]}</p>
+        </div>
       </div>
 
-      <div>
+      <h2>{data?.result?.[0]["title"]}과 비슷한 술입니다!</h2>
+      <div style={{ display: "flex" }}>
+        <div>
+          <img
+            src={data?.similar?.[0]}
+            style={{ width: "200px", height: "300px" }}
+          />
+          <p>{data?.result?.[0]["similar1"]}</p>
+        </div>
+        <div>
+          <img
+            src={data?.similar?.[1]}
+            style={{ width: "200px", height: "300px" }}
+          />
+          <p>{data?.result?.[0]["similar2"]}</p>
+        </div>
+        <div>
+          <img
+            src={data?.similar?.[2]}
+            style={{ width: "200px", height: "300px" }}
+          />
+          <p>{data?.result?.[0]["similar3"]}</p>
+        </div>
+      </div>
+
+      <h2>{data?.result?.[0]["title"]}과 잘 어울리는 안주입니다!</h2>
+      <div style={{ display: "flex" }}>
         <p>{data?.result?.[0]["snack1"]}</p>
         <p>{data?.result?.[0]["snack2"]}</p>
       </div>
