3a47327a30eb1d9ba1f28eca0a5fdbbeb9786b07
Seohyun Roh <radult951@gmail.com>
2022-04-28 21:54:14 +0900

    #7 update description

--- front/src/pages/WorldMapPage.js
+++ front/src/pages/WorldMapPage.js
@@ -1,45 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ComposableMap,
   Geographies,
   Geography,
   ZoomableGroup,
-  Marker,
 } from "react-simple-maps";
+import { Box, Typography } from "@mui/material";
 
+import * as Api from "../api";
 import Header from "../components/Header";
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
+const countryGrades = {
+  US: 5,
+  France: 4,
+  Italy: 4,
+  Portugal: 4,
+  Chile: 3,
+  Spain: 3,
+  "New Zealand": 3,
+  Argentina: 3,
+  Australia: 3,
+  Austria: 3,
+  "South Africa": 3,
+  Israel: 3,
+  Canada: 2,
+  Germany: 2,
+  Bulgaria: 2,
+  Greece: 2,
+  Lebanon: 1,
+  Hungary: 1,
+  Slovenia: 1,
+  Moldova: 1,
+  Turkey: 1,
+  Uruguay: 1,
+  Romania: 1,
+  Georgia: 1,
+  Mexico: 1,
+  England: 1,
+  Morocco: 1,
+  Croatia: 1,
+  Switzerland: 1,
+};
+
 function MapChart() {
-  const handleClick = e => {
-    console.log(e.target);
+  const [wine, setWine] = useState({});
+  const [description, setDescription] = useState("");
+  const [isExist, setIsExist] = useState(false);
+
+  const convertCountryName = name => {
+    switch (name) {
+      case "United States of America":
+        return "US";
+      case "United Kingdom":
+        return "England";
+      default:
+        return name;
+    }
+  };
+
+  const handleClick = async name => {
+    try {
+      const res = await Api.get(`worldMap/${convertCountryName(name)}`);
+      console.log(res.data.wine);
+      setDescription(res.data?.description[0]?.description);
+      setWine(res.data?.wine);
+      setIsExist(true);
+    } catch (err) {
+      console.log(err);
+      //isExist false로 체크.
+      //!isExist일 경우에만 description과 와인 카드 보여주기.
+    }
   };
 
   return (
     <>
       <Header />
-      <ComposableMap style={{ maxWidth: "1000px" }}>
-        <ZoomableGroup zoom={1}>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map(geo => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill="#DDD"
-                  stroke="#FFF"
-                  onClick={handleClick}
-                />
-              ))
-            }
-          </Geographies>
-          <Marker coordinates={[-74.006, 40.7128]}>
-            <circle r={4} fill="#F53" />
-          </Marker>
-        </ZoomableGroup>
-      </ComposableMap>
+      <Box
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ComposableMap
+          style={{
+            width: "1000px",
+            maxHeight: "400px",
+            margin: "20px 0",
+            border: "1px solid lightgray",
+          }}
+        >
+          <ZoomableGroup zoom={2}>
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map(geo => (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill="#DDD"
+                    stroke="#FFF"
+                    onClick={() =>
+                      handleClick(geo.properties.NAME, geo.properties.CONTINENT)
+                    }
+                  />
+                ))
+              }
+            </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+        {isExist && (
+          <Box
+            style={{
+              width: "960px",
+              minHeight: "120px",
+              border: "1px solid lightgray",
+              display: "flex",
+              alignItems: "center",
+              padding: "10px 20px",
+            }}
+          >
+            <Typography variant="subtitle1">{description}</Typography>
+          </Box>
+        )}
+      </Box>
     </>
   );
 }
