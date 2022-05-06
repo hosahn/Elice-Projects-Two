ba95b47fe53614d50b57171ffbbba5a354b7fa97
hj <bearn47@gmail.com>
2022-05-06 18:39:14 +0900

    하는중

--- front/src/pages/AboutPage.js
+++ front/src/pages/AboutPage.js
@@ -14,14 +14,67 @@ import Header from "../components/Header";
 import Cardlist from "../components/Cardlist";
 
 export default function AboutPage() {
+  const itemData = [
+    {
+      img: "logo512.png",
+      title: "이호산(팀장)",
+      author: "BE",
+      github: "https://naver.com",
+      something: "https://google.com",
+      rows: 2,
+      cols: 2,
+      featured: true,
+    },
+    {
+      img: "logo512.png",
+      title: "노서현",
+      author: "BE",
+      github: "https://naver.com",
+      something: "https://google.com",
+      rows: 2,
+      cols: 2,
+      featured: true,
+    },
+    {
+      img: "logo512.png",
+      title: "심은지",
+      author: "BE",
+      github: "https://naver.com",
+      something: "https://google.com",
+      rows: 2,
+      cols: 2,
+      featured: true,
+    },
+    {
+      img: "logo512.png",
+      title: "명하준",
+      author: "BE",
+      github: "https://github.com/hajun-myoung",
+      something: "mailto:fe.denver@gmail.com",
+      rows: 2,
+      cols: 2,
+      featured: true,
+    },
+    {
+      img: "logo512.png",
+      title: "김효진",
+      author: "BE",
+      github: "https://naver.com",
+      something: "https://google.com",
+      rows: 2,
+      cols: 2,
+      featured: true,
+    },
+  ];
+
   return (
     <>
       <Header />
-      <img src={`logo512.png`}></img>
-      
-      <Cardlist/>
-     <div>AboutPage</div>
-    
-     </>
+    <Grid container>
+      {itemData.map(item => {
+        return <Cardlist item={item}></Cardlist>;
+      })}
+      </Grid>
+    </>
   );
 }
