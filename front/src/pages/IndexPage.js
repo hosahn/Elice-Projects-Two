66387e3eb1cac4de56692564da3a7862dd0c2ff1
hajun <audgkwns44@gmail.com>
2022-05-07 02:50:00 +0900

    Udpate index:wireframe

--- front/src/pages/IndexPage.js
+++ front/src/pages/IndexPage.js
@@ -1,18 +1,63 @@
 import Header from "../components/Header";
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import { Link } from "@mui/material";
 
 export default function IndexPage() {
   return (
     <>
       <Header />
       <div className="index-container" />
-      <div className="index-content" style={{ marginTop: "100vh" }} />
+      <div
+        className="index-content"
+        style={{
+          marginTop: "40vh",
+          fontSize: "128px",
+          color: "#FFF",
+          textAlign: "right",
+        }}
+      >
+        <span style={{ background: "rgba(0, 0, 0, 0.6)" }}>
+          당신을 위한 와인,
+          <br />
+          Drunken Rabbit
+        </span>
+      </div>
+
+      {/* why we service? */}
+      <div
+        className="index-content"
+        style={{
+          background: "rgba(220, 20, 60, 0.7)",
+          color: "#FFF",
+          textAlign: "center",
+        }}
+      >
+        <span className="title">why we service?</span>
+      </div>
+
+      {/* master, 와인 추천해주세요 */}
+      <div
+        className="index-content"
+        style={{
+          marginTop: "200px",
+          padding: "30px 0",
+          height: "auto",
+          background: "rgba(220, 20, 60, 0.7)",
+          color: "#FFF",
+          textAlign: "center",
+        }}
+      >
+        <span className="title">
+          <Link
+            href="/main"
+            sx={{ textDecoration: "underline", color: "#FFF" }}
+          >
+            Master,
+            <br />
+            와인 추천해주세요!
+          </Link>
+        </span>
+      </div>
     </>
   );
 }
