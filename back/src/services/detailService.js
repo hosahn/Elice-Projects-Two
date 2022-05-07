bbbad220df535760fe9e7109dd5530875a181c75
hajun <audgkwns44@gmail.com>
2022-05-08 08:57:14 +0900

    Update backcode for detailpage front

--- back/src/services/detailService.js
+++ back/src/services/detailService.js
@@ -1,4 +1,5 @@
 import { Wine } from "../db/index.js";
+import { User } from "../db/index.js";
 
 class detailService {
   //와인의 index를 받아 와인 객체 하나를 return 받습니다. 결과값은 배열로 보내지기 때문에 항상 result[0]이 해당 와인이 됩니다.
@@ -13,10 +14,21 @@ class detailService {
     for (let i = 0; i < similarWine.length; i++) {
       const wineName = similarWine[i];
       const tmp = await Wine.FindByWineName({ wineName });
-      result.push(tmp[0]["image"]);
+      const result2 = [tmp[0]["image"], tmp[0]["index"]]
+      result.push(tmp[0]);
     }
     return result;
   }
   //설명추가
+  static async clickWine({ index, user_id }) {
+    const hasWine = await User.hasFavoriteWine({ index, user_id });
+    if (hasWine) {
+      return await User.removeFavoriteWine({
+        user_id,
+        index,
+      });
+    }
+    return await User.addFavoriteWine({ user_id, index });
+  }
 }
 export { detailService };
